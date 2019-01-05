293c75c614e1cd7140bc0686abf6598598dcb376
Nakaji Kohki <boomerang.cube@gmail.com>
2019-01-06 08:09:58 +0900

    Add prefab references

---
+++ UnityAutoIconBuilder/Assets/UnityAutoIconBuilder/Scripts/GenSystemPrefab.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GenSystemPrefab : MonoBehaviour
+{
+    public Text Version = null;
+    public string VersionFormat = "Ver {0}";
+    public Camera Camera = null;
+}
